1150d63869a1838343d3dcfdac47d71021660420
Valgaal <goldenfire_95@yahoo.com>
2019-01-27 09:50:31 +0300

    models

--- app/src/main/java/com/example/nikita/javathon/UI/NewPartyActivity/MemberModel.java
+++ app/src/main/java/com/example/nikita/javathon/UI/NewPartyActivity/MemberModel.java
@@ -1,4 +1,30 @@
 package com.example.nikita.javathon.UI.NewPartyActivity;
 
+import com.google.gson.annotations.Expose;
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class MemberModel {
+
+    @Expose
+    @Getter
+    @Setter
+    private long id;
+
+    @Expose
+    @Getter
+    @Setter
+    private String nickname;
+
+    @Expose
+    @Getter
+    @Setter
+    private double balance;
+
+    @Expose
+    @Getter
+    @Setter
+    private String cardNumber;
+
 }
--- app/src/main/java/com/example/nikita/javathon/UI/PartyList/PartyListModel.java
+++ app/src/main/java/com/example/nikita/javathon/UI/PartyList/PartyListModel.java
@@ -1,4 +1,44 @@
 package com.example.nikita.javathon.UI.PartyList;
 
+import com.example.nikita.javathon.UI.NewPartyActivity.MemberModel;
+import com.example.nikita.javathon.UI.ProductsList.ProductsModel;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class PartyListModel {
+
+    @SerializedName("id")
+    @Expose
+    @Getter
+    @Setter
+    private long id;
+
+    @SerializedName("name")
+    @Expose
+    @Getter
+    @Setter
+    private String name;
+
+    @SerializedName("description")
+    @Expose
+    @Getter
+    @Setter
+    private String description;
+
+    @SerializedName("members")
+    @Expose
+    @Getter
+    @Setter
+    private List<MemberModel> members;
+
+    @SerializedName("items")
+    @Expose
+    @Getter
+    @Setter
+    private List<ProductsModel> items;
 }
